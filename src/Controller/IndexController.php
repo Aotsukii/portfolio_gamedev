d2039ed11f0cde3aee5dd241253975be4f81c9bf
Elie Gazel <elie@gazel.net>
2020-09-10 17:46:16 +0200

    Routing + pages empty twig

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -16,4 +16,54 @@ class IndexController extends AbstractController
             'controller_name' => 'IndexController',
         ]);
     }
+
+    /**
+     * @Route("/career", name="career")
+     */
+    public function career()
+    {
+        return $this->render('pages/career.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    /**
+     * @Route("/productions", name="productions")
+     */
+    public function productions()
+    {
+        return $this->render('pages/productions.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    /**
+     * @Route("/references", name="references")
+     */
+    public function references()
+    {
+        return $this->render('pages/references.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    /**
+     * @Route("/hobbies", name="hobbies")
+     */
+    public function hobbies()
+    {
+        return $this->render('pages/hobbies.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('pages/contact.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
 }
